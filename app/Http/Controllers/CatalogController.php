cb6b3405381f01715cd6f182caa39bd4bf8bfa55
wael walid <wael.walid91@gmail.com>
2022-11-09 02:23:37 +0200

    add catalog route

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -5,14 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Catalog;
 use Exception;
+use Ramsey\Uuid\Type\Integer;
 
 class CatalogController extends Controller
 {
     function index(Request $request)
     {
         try {
-            $catalogs = Catalog::get();
+            $catalogs = Catalog::paginate(5);
             return response()->json(['data' => $catalogs]);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+
+    function show($id)
+    {
+        try {
+            $catalog = Catalog::find($id);
+            return response()->json([$catalog]);
         } catch (Exception $e) {
             throw $e;
         }
--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/' ,  function() { 
+Route::get('/check' ,  function() { 
     return response()->json(["Hello! you are using catalog service and it is running successfully :D "]);
 });
 Route::group(['namespace' => '\\App\Http\Controllers\\'], function(){
-  Route::get('index', 'CatalogController@index');
+  Route::get('/', 'CatalogController@index');
+  Route::get('/{id}', 'CatalogController@show');
 });
